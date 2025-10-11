1ab7463ea3887e6036decdecda882701a9670496
maybe-asdf <brtekko7809@gmail.com>
2025-10-12 00:57:57 +0200

    initial release feeling PROUD

---
+++ client.py
@@ -0,0 +1,61 @@
+import asyncio
+import json
+import websockets
+from prompt_toolkit.patch_stdout import patch_stdout
+from prompt_toolkit import PromptSession
+
+ver = "v000001"
+print(fr"""
+      __                               
+ ____/ /  ___ ____ ____ ___  __ ____ __
+/ __/ _ \/ _ `/ _ `(_-</ _ \/ // /\ \ /
+\__/_//_/\_,_/\_,_/___/_//_/\_, //_\_\ 
+      {ver} client         /___/       
+      """)
+
+async def send_loop(websocket, username, session):
+    while True:
+        msg_text = await session.prompt_async(f"[{username}] - ")  # no prompt character
+        message = {"sender": username, "content": msg_text}
+        await websocket.send(json.dumps(message))
+
+async def receive_loop(websocket, username):
+    while True:
+        try:
+            response = await websocket.recv()
+            try:
+                # Try parsing as JSON
+                message = json.loads(response)
+                # Skip messages sent by self
+                if message.get("sender") == username:
+                    continue
+                sender = message.get("sender")
+                content = message.get("content")
+                print(f"[{sender}] - {content}")
+            except json.JSONDecodeError:
+                # Plain text from server (like join notifications)
+                print(response)
+        except websockets.ConnectionClosed:
+            print("Connection closed")
+            break
+
+async def main():
+    address = input("ip to connect : ")
+    x = address.split(":")
+    uri = f"ws://{address}" if len(x) == 2 else f"ws://{address}:6741"
+    username = input("username to log in as : ")
+
+    async with websockets.connect(uri) as websocket:
+        # Send join message
+        join_msg = {"sender": username, "content": "join;"}
+        await websocket.send(json.dumps(join_msg))
+
+        session = PromptSession()
+        with patch_stdout():  # ensures prints from receive_loop don't break input
+            await asyncio.gather(
+                send_loop(websocket, username, session),
+                receive_loop(websocket, username)
+            )
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ server.py
@@ -0,0 +1,69 @@
+ver = "v000001"
+print(fr"""
+      __                               
+ ____/ /  ___ ____ ____ ___  __ ____ __
+/ __/ _ \/ _ `/ _ `(_-</ _ \/ // /\ \ /
+\__/_//_/\_,_/\_,_/___/_//_/\_, //_\_\ 
+      {ver} server         /___/       
+      """)
+def log(thing):
+    print(thing)
+log("Importing asyncio")
+import asyncio
+log("Importing json")
+import json
+log("Importing websockets")
+from websockets.asyncio.server import serve
+log("Finished importing")
+clients = set()
+senders = set()
+log("Defining..")
+async def handler(websocket):
+    join_msg_string = await websocket.recv()
+    join_msg = json.loads(join_msg_string)
+
+    sender_name = join_msg["sender"]
+    if join_msg["content"] != "join;":
+        log(f"Someone tried to log in without a valid content. Denied access")
+        await websocket.send("Invalid authentication/username. We are done here")
+        return
+
+    if sender_name in senders:
+        log(f"Someone tried to log in by the taken username of {sender_name}. Denied access")
+        await websocket.send("Taken username. We are done here")
+        return
+
+    senders.add(sender_name)
+    clients.add(websocket)
+
+    # Notify everyone that this user joined
+    for client in clients.copy():
+        try:
+            await client.send(f"- {sender_name} - just joined -")
+        except:
+            clients.discard(client)
+            senders.discard(sender_name)
+
+    try:
+        async for message_string in websocket:
+            message = json.loads(message_string)
+            message_content = message["content"]
+            sender = message["sender"]
+
+            for client in clients.copy():
+                try:
+                    await client.send(json.dumps({"sender": sender, "content": message_content}))
+                except:
+                    clients.discard(client)
+                    senders.discard(sender)
+    finally:
+        clients.discard(websocket)
+        senders.discard(sender_name)
+
+async def main():
+    log("Open for business!")
+    async with serve(handler, "localhost", 6741):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
